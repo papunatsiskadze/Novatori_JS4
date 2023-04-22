235d1d51a68ea565401e6d660f712dd1a2d4be68
papunatsiskadze <papunaciskadze@gmail.com>
2023-04-22 22:40:21 +0400

    almost finished

---
+++ Js4.js
@@ -0,0 +1,115 @@
+// let userFirstName = "Lalo" ;
+
+// console.log(`salami ${userFirstName}!`)
+
+
+
+
+
+// let userBalance = 300;
+// let cartTotal = 300;
+
+// if(userBalance >= cartTotal){
+//     console.log("გადახდა წარმატებით შესრულდა")
+// }
+// else{
+//     console.log(`თქვენ არ გაქვთ საკმარისი თანხა,გთხოვთ დამატებით ჩარიცხოთ ${cartTotal - userBalance} ლარი`)
+// }
+
+
+
+
+
+// let maxScore = 100;
+// let barrier = 50;
+// let studentScore = 120;
+
+// if(studentScore >= barrier){
+//     console.log(`გილოცავთ თქვენ გადალახეთ ${barrier} ქულიანი ბარიერი ${(studentScore/maxScore) * 100} ქულით`)
+// }
+// else{
+//     console.log(`ვწუხვარ,თქვენ ვერ გადალახეთ ${barrier} ქულიანი ბარიერი ${(studentScore/maxScore) * 100} ქულით`)
+// }
+
+
+
+
+// let shippingIsPossible = true;    
+
+// console.log(`თქვენს მისამართზე მიტანის სერვისი ${shippingIsPossible ? "" : "არ"} არის`);
+
+
+
+
+
+
+// let userLoggedIn = false;
+// let userIsFemale = true;
+// let userFirstName = "ანა";
+
+
+// if(userLoggedIn){
+//     console.log(`სალამი ${userIsFemale ? "ქალბატონო" : "ბატონო"} ${userFirstName}`)
+// }
+// else{
+//     console.log(`დროებით ${userIsFemale ? "ქალბატონო" : "ბატონო"} ${userFirstName}`)
+// }
+
+
+
+
+
+
+// let userQuestions = [] ;
+
+// userQuestions[24] = "25th question" ;
+
+// if(userQuestions.length >= 24){
+//     console.log("შემდეგი კითხვის დასმას შეძლებთ სამ საათში")
+// }
+
+
+
+
+
+
+
+// let userWeights = [105, 100, 95, 90];
+// let goal = 85;
+
+
+// console.log(`შედეგის მიღწევამდე დაგრჩა ${userWeights[userWeights.length - 1] - goal} კილოგრამი`);
+
+
+
+
+
+// let gigosResults = [50, 62, 67] ;
+
+// if(gigosResults[gigosResults.length - 1] <= gigosResults[0]){
+//     console.log("უხ გიგოო გიგოო!")
+// }
+// else if((gigosResults[gigosResults.length - 1] - gigosResults[0]) <= 10){
+//     console.log("რავიცი ნიჭიერია მაგრამ ზარმაცი")
+// }
+// else{
+//     console.log("კარგი შვილი გყავთ ქალბატონო,ნუ ნერვიულობთ")
+// }
+
+
+
+
+
+// let gigosResults = [60, 55, 57] ;
+
+// if((gigosResults[gigosResults.length - 1] - gigosResults[0]) >= 5){
+//     console.log("კარგი შვილი გყავთ ქალბატონო,ნუ ნერვიულობთ")
+// }
+// else if((gigosResults[gigosResults.length - 1] - gigosResults[0]) >= 2 ){
+//     console.log("ნიჭიერია მაგრამ ზარმაცი")
+// }
+
+
+
+
+
